361542e815b4e427b67ad4fc6575c68fe461e48c
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-10-04 10:54:09 +0000

    [Add]投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,27 @@
 class BooksController < ApplicationController
+    def index
+        @book = Book.new
+        @book = Book.all
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def create
+        book = Book.new(list_params)
+        book.save
+        redirect_to '/show'
+    end
+    
+    def destroy
+    end 
+
+  private
+
+    def list_params
+    params.require(:book).permit(:title, :body)
+    end
 end
