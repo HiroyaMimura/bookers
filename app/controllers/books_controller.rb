5d9918cf6db1098c1e2879c7c32674051d861530
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-10-05 13:03:41 +0000

    [Add]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,44 @@
 class BooksController < ApplicationController
     def index
-        @book = Book.new
+        @new = Book.new
         @book = Book.all
     end
-    
+
     def show
+        @book = Book.find(params[:id])
     end
-    
+
     def edit
+        @book = Book.find(params[:id])
     end
-    
+
     def create
-        book = Book.new(list_params)
-        book.save
-        redirect_to '/show'
+        @new = Book.new(list_params)
+     if @new.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@new.id)
+     else
+        @book = Book.all
+        render:index
+     end
     end
-    
+
+    def update
+        @book = Book.find(params[:id])
+    if  @book.update(list_params)
+        flash[:notice] = "Book was successfully updated."
+        redirect_to book_path(@book.id)
+    else
+        render:edit
+    end
+    end
+
     def destroy
-    end 
+        book = Book.find(params[:id])
+        book.destroy
+        flash[:notice] = "Book was successfully destroyed."
+        redirect_to '/books'
+    end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  get 'index' => 'books#index'
-  get 'show' =>'books#show'
-  post 'books' => 'books#create'
-  resources:lists
+  get '/' => 'homes#top'
+  get 'books/:id' => 'books#show', as:'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_list'
+  patch 'books/:id' => 'books#update', as:'update_list'
+  delete 'books/:id' => 'books#destroy', as:'destroy_list'
+  resources:books
 end
