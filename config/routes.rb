889415e603e0c345548967e12c6d044b142ecfa8
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-10-06 04:56:06 +0000

    [Finish]課題２

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'homes#top'
+  root to: 'homes#top'
   get 'books/:id' => 'books#show', as:'book'
   get 'books/:id/edit' => 'books#edit', as:'edit_list'
   patch 'books/:id' => 'books#update', as:'update_list'
